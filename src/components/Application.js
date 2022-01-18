8cde30b089170907e82890be96d90d24fed8b3dd
Emily <emilyabigailwaters@gmail.com>
2022-01-18 10:09:13 -0800

    Created Appointment directory with index for displaying Appointments, created styles.scss with styles for Appointment and updated stories to include Appointments stories

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import DayList from "./DayList";
+// import InterviewerList from "./InterviewerList";
 
 import "components/Application.scss";
-import InterviewerList from "./InterviewerList";
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  // const [interviewer, setInterviewer] = useState("Sylvia Palmer");
   const days = [
     {
       id: 1,
@@ -23,6 +24,14 @@ export default function Application(props) {
       spots: 0,
     },
   ];
+  // const interviewers = [
+  //   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  //   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  //   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  //   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  //   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
+  // ];
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -43,7 +52,11 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {/* <InterviewerList /> */}
+        {/* <InterviewerList
+          interviewers={interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        /> */}
       </section>
     </main>
   );
