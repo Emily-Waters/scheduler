ea5483fa6a641d4daea77b88c9ca257ed39365af
Emily <emilyabigailwaters@gmail.com>
2022-01-18 11:36:02 -0800

    Created Status.jsx in Appointment/, updated stories to include Status story

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import Confirm from "./Confirm";
+import Empty from "./Empty";
 import Header from "./Header";
 import Show from "./Show";
+import Status from "./Status";
 import "./styles.scss";
 
 export default function Appointment(props) {
