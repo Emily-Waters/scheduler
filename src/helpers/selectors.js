d836d2c922c1abbc4df087a7aef3936f9abcf0a2
Emily <emilyabigailwaters@gmail.com>
2022-01-19 10:49:07 -0800

    created getInterview in selectors.js, implemented functionality, added tests for getInterview in selectors.test.js

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,15 @@
-export default function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
   return state.days
     .filter((days) => days.name === day)
     .flatMap((day) => day.appointments)
     .map((dayApps) => state.appointments[dayApps]);
+}
+
+export function getInterview(state, interview) {
+  const newInterview = interview;
+  if (!newInterview) {
+    return newInterview;
+  }
+  newInterview.interviewer = state.interviewers[interview.interviewer];
+  return newInterview;
 }
