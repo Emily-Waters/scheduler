f3a7ce14c057b638e2241769d5cebf2e3f48810a
Emily <emilyabigailwaters@gmail.com>
2022-01-19 15:22:09 -0800

    Implemented getInterviewersForDay in helpers/selectors.js, implemented testing for getInterviewersForDay in selectors.test.js. Added comments

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -9,21 +9,33 @@ import Show from "./Show";
 import Status from "./Status";
 // Styles
 import "./styles.scss";
+// Helpers
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
   const { time, interview } = props;
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const CONFIRM = "CONFIRM";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? (
-        <Show
-          student={interview.student}
-          interviewer={interview.interviewer.name}
-        />
-      ) : (
-        <Empty />
+      {mode === SHOW && (
+        <Show student={interview.student} interviewer={interview.interviewer} />
       )}
-      {/* {props.time ? "Appointment at " + props.time : "No Appointments"} */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={() => transition(CONFIRM)}
+          onCancel={() => back()}
+        />
+      )}
+      {mode === CONFIRM && <Confirm />}
     </article>
   );
 }
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,8 @@
 export function getAppointmentsForDay(state, day) {
   return state.days
-    .filter((days) => days.name === day)
-    .flatMap((day) => day.appointments)
-    .map((dayApps) => state.appointments[dayApps]);
+    .filter((days) => days.name === day) // Filters day array returning days matching day arg
+    .flatMap((day) => day.appointments) // Seperates appointment ids in filtered day array into a new array
+    .map((dayApps) => state.appointments[dayApps]); // Maps a new array of appointments by appointment id
 }
 
 export function getInterview(state, interview) {
@@ -12,4 +12,13 @@ export function getInterview(state, interview) {
         ...interview,
         interviewer: state.interviewers[interview.interviewer],
       };
+}
+
+export function getInterviewersForDay(state, day) {
+  return state.days
+    .filter((days) => days.name === day) // Filters day array returning days matching day arg
+    .flatMap((day) => day.appointments) // Seperates appointment ids in filtered day array into a new array
+    .map((dayApps) => state.appointments[dayApps].interview) // Maps a new array of interview objects by appointment id
+    .filter((interviews) => interviews) // Filters out null interviews
+    .map((interviews) => state.interviewers[interviews.interviewer]); // Maps new array of interviewers by interviewer id
 }
