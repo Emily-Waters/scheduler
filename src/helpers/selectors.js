0d2af034a5ba6489d1ea6e08b678cb0de63e605f
Emily <emilyabigailwaters@gmail.com>
2022-01-18 21:57:33 -0800

    changed export to export default

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   return state.days
     .filter((days) => days.name === day)
     .flatMap((day) => day.appointments)
