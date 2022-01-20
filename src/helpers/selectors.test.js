2cd1c3463a09eab8c56b6b7e9e7db89213e146bd
Emily <emilyabigailwaters@gmail.com>
2022-01-19 16:04:48 -0800

    Improved testing for selectors by varying data

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -14,7 +14,7 @@ const state = {
     {
       id: 2,
       name: "Tuesday",
-      appointments: [4, 5],
+      appointments: [3, 5],
     },
   ],
   appointments: {
@@ -23,7 +23,7 @@ const state = {
     3: {
       id: 3,
       time: "2pm",
-      interview: { student: "Archie Cohen", interviewer: 2 },
+      interview: { student: "Archie Cohen", interviewer: 1 },
     },
     4: { id: 4, time: "3pm", interview: null },
     5: {
@@ -58,7 +58,7 @@ test("getAppointmentsForDay returns an array with a length matching the number o
 
 test("getAppointmentsForDay returns an array containing the correct appointment objects", () => {
   const [first, second] = getAppointmentsForDay(state, "Tuesday");
-  expect(first).toEqual(state.appointments["4"]);
+  expect(first).toEqual(state.appointments["3"]);
   expect(second).toEqual(state.appointments["5"]);
 });
 
@@ -98,7 +98,8 @@ test("getInterviewersForDay returns an array with a length matching the number o
 
 test("getInterviewersForDay returns an array containing the correct appointment objects", () => {
   const [first, second] = getInterviewersForDay(state, "Tuesday");
-  expect(first).toEqual(state.interviewers["2"]);
+  expect(first).toEqual(state.interviewers["1"]);
+  expect(second).toEqual(state.interviewers["2"]);
 });
 
 test("getInterviewersForDay returns an empty array when the days data is empty", () => {
@@ -110,45 +111,3 @@ test("getInterviewersForDay returns an empty array when the day is not found", (
   const result = getInterviewersForDay(state, "Wednesday");
   expect(result.length).toEqual(0);
 });
-
-// const state = {
-//   days: [
-//     {
-//       id: 1,
-//       name: "Monday",
-//       appointments: [1, 2, 3],
-//     },
-//     {
-//       id: 2,
-//       name: "Tuesday",
-//       appointments: [4, 5],
-//     },
-//   ],
-//   appointments: {
-//     1: { id: 1, time: "12pm", interview: null },
-//     2: { id: 2, time: "1pm", interview: null },
-//     3: {
-//       id: 3,
-//       time: "2pm",
-//       interview: { student: "Archie Cohen", interviewer: 2 },
-//     },
-//     4: { id: 4, time: "3pm", interview: null },
-//     5: {
-//       id: 5,
-//       time: "4pm",
-//       interview: { student: "Chad Takahashi", interviewer: 2 },
-//     },
-//   },
-//   interviewers: {
-//     1: {
-//       id: 1,
-//       name: "Sylvia Palmer",
-//       avatar: "https://i.imgur.com/LpaY82x.png",
-//     },
-//     2: {
-//       id: 2,
-//       name: "Tori Malcolm",
-//       avatar: "https://i.imgur.com/Nmx0Qxo.png",
-//     },
-//   },
-// };
